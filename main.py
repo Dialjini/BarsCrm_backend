36d685938bac3baeffb7f8c8414f2c29be8489de
Dialjini <danil_kustov@mail.ru>
2019-10-29 21:47:12 +0700

    Init DataBase

---
+++ main.py
@@ -0,0 +1,1 @@
+from BARS import app
---
+++ static/script/script.js
@@ -0,0 +1,37 @@
+var gifCount = 1;
+var delta = 500;
+var Timer;
+var stopped = true;
+
+function stopGif() {
+    clearTimeout(Timer);
+    stopped = true;
+}
+
+function faster() {
+    delta = delta / 1.3;
+}
+
+function slower() {
+    delta = delta * 1.3;
+}
+
+function start() {
+    if (stopped == true) {
+        stopped = false;
+        Timer = setTimeout("anime()", delta);
+    }
+
+}
+
+function anime() {
+    console.log(document.getElementById("field"));
+    var element = document.getElementById('field');
+    element.setAttribute('src', '/static/img/s' + gifCount.toString() + '.gif');
+    if (gifCount != 12) {
+        gifCount = gifCount + 1;
+    } else {
+        gifCount = 1;
+    }
+    Timer = setTimeout("anime()", delta);
+}
