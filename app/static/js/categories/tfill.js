89c2f791230d00739e7601144a084b9565c26841
Daniil Soldatov <of.swg.csgo@gmail.com>
2020-02-03 00:55:36 +0700

    hhiihihih

--- app/static/js/categories/tfill.js
+++ app/static/js/categories/tfill.js
@@ -345,6 +345,8 @@ let rowFilling = (object, id, table) => {
             let payment_date = selectTableData[i].delivery.Payment_date;
             if (selectTableData[i].delivery.Name == 'Транзит') {
                 full_name = `${selectTableData[i].delivery.Name} из ${selectTableData[i].delivery.Stock} в ${selectTableData[i].delivery.Contact_Number}`;
+            } else {
+                full_name = selectTableData[i].delivery.Name;
             }
             const name = [selectTableData[i].delivery.Date, full_name, selectTableData[i].delivery.Stock, carrier_name, selectTableData[i].delivery.Customer, customer == 'ООО' ? '' : price_without_vat, returnSpaces(amount), status, payment_date == '' ? 'Не указано' : payment_date];
             for (let j = 0; j < name.length; j++) {
