03f163eeeeb296d5fc0585e29e4a6d432ff1f2c4
Dialjini <danil_kustov@mail.ru>
2020-02-10 16:27:43 +0700

    modify docs

--- app/routes.py
+++ app/routes.py
@@ -147,7 +147,7 @@ def index():
         print("Not logged in")
 
     if 'username' in session:
-        return render_template('index.html', last_update=2431)
+        return render_template('index.html', last_update=2432)
     else:
         return render_template('login.html', last_update=1883)
 
