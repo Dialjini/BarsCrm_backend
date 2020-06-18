e43ab8af7828e82ba9e2cf8fb5a166173f5bb6d9
Daniil Soldatov <of.swg.csgo@gmail.com>
2020-06-18 17:24:42 +0800

    Fix

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -487,8 +487,9 @@ function saveInfoCard(id, close = false, elem = null, checkINN = 'none') {
                 let name = idData[`client_name`];
                 console.log(idData);
                 let area = idData[`client_area`];
+                let region = idData[`client_region`];
                 for (let client of clients) {
-                    if (client.Rayon == area && client.Name == name && client.id != id.split('_')[3]) {
+                    if (client.Oblast == region && client.Rayon == area && client.Name == name && client.id != id.split('_')[3]) {
                         $('#preloader').remove();
                         return $('.page').append($('<div>', { class: 'background' }).add(`
                             <div class="modal_select">
@@ -583,8 +584,9 @@ function saveInfoCard(id, close = false, elem = null, checkINN = 'none') {
                         })
                         let name = idData[`client_name`];
                         let area = idData[`client_area`];
+                        let region = idData[`client_region`];
                         for (let client of clients) {
-                            if (client.Rayon == area && client.Name == name && client.id != id.split('_')[3]) {
+                            if (client.Oblast == region && client.Rayon == area && client.Name == name && client.id != id.split('_')[3]) {
                                 $('#preloader').remove();
                                 return $('.page').append($('<div>', { class: 'background' }).add(`
                                     <div class="modal_select">
